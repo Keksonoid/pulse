b343fa7520a104a3ac9f189f2889f1e02e1dbb1d
Kirill <keksonoid98@gmail.com>
2020-04-18 13:20:46 +0600

    add map and add modals

--- src/js/script.js
+++ src/js/script.js
@@ -19,17 +19,7 @@ $(document).ready(function(){
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
-
-    // $('.catalog-item__link').each(function(i) {
-    //     $(this).on('click', function(e) {
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     });
-    // });
-
     
-
     function togleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -43,5 +33,23 @@ $(document).ready(function(){
     togleSlide('.catalog-item__link');
     togleSlide('.catalog-item__back');
 
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 
 });
